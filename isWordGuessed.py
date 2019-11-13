23959dc6e8ed76638f139da652be8ce0bdb5f589
Kyle Prestado <57376389+kylerossece@users.noreply.github.com>
2019-11-13 21:09:32 +0800

    Update isWordGuessed.py

--- isWordGuessed.py
+++ isWordGuessed.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 def isWordGuessed(secretWord,lettersGuessed):
     for x in secretWord:   
-      if x not in lettersGuessed:
+      if x in lettersGuessed:
+          return True
+      else:
           return False
-    return True
         
     
